250d1b63494342d47617e477bb38d7d5ae86cf52
Jeremy Green <hello@jeremy.green>
2025-04-27 06:49:43 -0400

    Remove `debug_lines.sh` script as it is no longer needed.

--- debug_lines.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# A simple script to show line numbers
-nl -w4 -s' ' /Users/jeremy/Development/hacks/node-mealie/src/services/media.ts | sed -n '40,50p'
-echo "---"
-nl -w4 -s' ' /Users/jeremy/Development/hacks/node-mealie/src/services/media.ts | sed -n '70,80p'
-echo "---"
-nl -w4 -s' ' /Users/jeremy/Development/hacks/node-mealie/src/services/media.ts | sed -n '100,120p'
